564a79db1f53fbcbcaf8e15dd3894298a4684650
Justin Balagtas <justinseebee@hotmail.com>
2024-06-15 23:07:53 -0400

    complete mobile design

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-
   return (
     <>
       <main>
@@ -20,9 +19,57 @@ function App() {
             <p className="cardDate">00/00</p>
           </div>
         </section>
-        <section className="form">form</section>
+        <section className="form">
+          <form action="">
+            <div className="formRow">
+              <label htmlFor="cardHolder">CARDHOLDER NAME</label>
+              <input
+                type="text"
+                name="cardHolder"
+                id="cardHolder"
+                placeholder="e.g Jane Appleseed"
+              />
+            </div>
+
+            <div className="formRow">
+              <label htmlFor="cardNumber">CARD NUMBER</label>
+              <input type="number" name="cardNumber" id="cardNumber" placeholder="e.g. 1234 5678 9123 0000"/>
+            </div>
+
+            <div className="formCol">
+
+              <div className="formRow">
+                <label htmlFor="">EXP. DATE (MM/YY)</label>
+                <div className="expInputs">
+                  <input
+                    type="text"
+                    name="cardMM"
+                    id="cardMM"
+                    placeholder="MM"
+                    // size={4}
+                  />
+                  <input
+                    type="text"
+                    name="cardYY"
+                    id="cardYY"
+                    placeholder="YY"
+                    // size={4}
+                  />
+                </div>
+              </div>
+
+              <div className="formRow">
+                <label htmlFor="cardCVC">CVC</label>
+                <input type="text" name="cardCVC" id="cardCVC" placeholder="e.g. 123"/>
+              </div>
+
+              
+            </div>
+            <button type="button" className="conBtn">Confirm</button>
+          </form>
+
+        </section>
       </main>
-      
     </>
   );
 }
